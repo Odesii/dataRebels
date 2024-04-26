0dd2a319abc4ef9051d441c2c04cadc9abaad5c1
RTAKA808 <106231361+RTAKA808@users.noreply.github.com>
2024-04-25 21:33:48 -0700

    almost done with vanilla combat system

--- assets/js/combatSystem.js
+++ assets/js/combatSystem.js
@@ -15,10 +15,19 @@ class CombatSystem{
     constructor(player,enemy){
         this.player=player
         this.enemy=enemy
+        this.turns=0;
         this.playerMove=true;
         this.enemyMove=true;
         this.status; //0 no combat /1 combat
         this.oneTurnSkill=false
+    }
+
+    turn(){
+        return this.turns
+    }
+    nextTurn(){
+        this.turns++;
+    return this.turns
     }
 
     combatStart(){
@@ -38,19 +47,48 @@ class CombatSystem{
                     this.oneTurnSkill=true
                     break
             }
+        if(attacker===this.player){
+            this.enemyMove=false
+        }else{
+            this.playerMove=false
         }
+        if(this.enemyMove===false && this.playerMove===false){
+            this.nextTurn;
+            this.enemyMove=true;
+            this.playerMove=true;
+            if(this.oneTurnSkill===true){
+                this.resetStatsAfterMove()
+            }
         }
+        }else{
+            console.log(`end of combat`)
+        }
+        this.winLoss();
+        
+}
 
-
-    }
+    
     winLoss(){
         if(this.player.hp<=0){
-            this.status=0
+            this.status=0;
         }
-        if(this.enemy.hp<=0){
-            this.status=0
-        }
+            if(this.enemy.hp<=0){
+                this.status=0;
+            }
     }
 
+    endPlayerMove(){
+        this.enemyMove=false;
+        return this.enemyMove;
+    }
+    enemyMove(){
+        this.startTurn(player, enemy)
+        return this.playerMove
+    }
+    oneTurnSkillReset(){
+        this.player.def=this.player.defBase;
+        this.enemy.def=this.player.defBase;
+        this.oneTurnSkill=false
+    }
 
 }
