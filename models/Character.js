b3459914dbb3361880bb29499e3117bb6feb0bea
RTAKA808 <106231361+RTAKA808@users.noreply.github.com>
2024-05-06 11:49:42 -0700

    server js done

---
+++ models/Character.js
@@ -0,0 +1,48 @@
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '../config/connection';
+
+class Character extends Model {}
+
+Character.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    hp:{
+      type:DataTypes.STRING,
+      allowNull: false,
+    },
+    attack: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    defense:{
+        type: DataTypes.INTEGER,
+        allowNull:false
+    },
+
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'character',
+  }
+);
+
+module.exports = Character;
