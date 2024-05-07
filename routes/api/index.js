796a6c98fbdc169f17b101f0bfc8dedf4e884baf
Odesii <alic.madd@gmail.com>
2024-05-07 17:22:30 -0400

    ٩(^ᗜ^ )و ´- added Auth to home and set register as loggedin

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const enemyRoutes = require('./enemyRoutes');
 const gameRoutes = require('./gameRoutes');
 
 router.use('/users', userRoutes);
-router.use('/enemies', enemyRoutes);
 router.use('/games', gameRoutes);
 
 module.exports = router;
--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User, Character, Enemy, Game } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/',  async (req, res) => {
+router.get('/', withAuth,  async (req, res) => {
     res.render('homepage', { loggedIn: req.session.loggedIn })
 })
 
