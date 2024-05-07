851659d4bed9e74bb129eecbb7e3b98d5c985006
Odesii <alic.madd@gmail.com>
2024-05-07 16:26:43 -0400

    health bar testing ദ്ദി ˉ͈̀꒳ˉ͈́ )✧

--- public/js/render.js
+++ public/js/render.js
@@ -4,12 +4,12 @@ import { EffectComposer } from 'three/EffectComposer';
 import { RenderPass } from 'three/RenderPass';
 import { UnrealBloomPass } from 'three/UnrealBloomPass';
 import { CSS2DRenderer, CSS2DObject} from 'three/CSS2DRenderer';
-// import { label } from 'three/examples/jsm/nodes/Nodes.js';
-// import anime from 'animejs';
-import { createTextLabels } from './extra/testText.js';
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -211,38 +211,31 @@ document.querySelector('#attack').addEventListener('click', (event) => {
       }, 400);
   });
 
+// player take damage
+document.querySelector('#health-bar').addEventListener('change', (event) => {
+    event.preventDefault();
+    const cardMat = card.material;
 
+    cardMat.emissive.set(0xa44141)
+    
+    console.log('Animation started!')
+    anime({
+        targets: cardMat, 
+        emissiveIntensity: [0.5, 0.1],
+        duration: 150,
+        easing: 'easeInOutSine',
+        direction: 'alternate',
+        loop: 2,
+        delay: 50,
+        complete: () => {
+          console.log('Animation ended!');
+        }
+      });
 
-// hp bar test
-// Create an HTML element
-const testText = document.createElement('div');
-testText.textContent = 'TEST TEST TEST';
-testText.style.marginTop = '-1em'; // Center vertically
-
-// Wrap it in a CSS2DObject
-const labelObject = new CSS2DObject(testText);
-labelObject.position.set(-5.5, 1, -10.0); // Position in 3D space 
-scene.add(labelObject);
-
-// Set up the CSS2DRenderer
-const labelRenderer = new CSS2DRenderer();
-labelRenderer.setSize(window.innerWidth, window.innerHeight);
-labelRenderer.domElement.style.position = 'absolute';
-labelRenderer.domElement.style.top = '0px';
-labelRenderer.domElement.style.pointerEvents = 'none';
-document.body.appendChild(labelRenderer.domElement);
-
-
-
-const textCanvas = document.createElement('canvas');
-const textContext = textCanvas.getContext('2d');
-textContext.font = '14px Arial';
-textContext.fillText('Hello, world!', 1, 2, 0);
-
-const textTexture = new THREE.Texture(textCanvas);
-const textSprite = new THREE.Sprite(new THREE.SpriteMaterial({ map: textTexture }));
-scene.add(textSprite);
-
+      setTimeout(() => {
+        cardMat.emissive.set(0x000000)
+      }, 400);
+  });
 
 
 const composer = new EffectComposer(renderer);
@@ -273,7 +266,8 @@ function animate() {
         }
     }
     // render the text
-    labelRenderer.render(scene, camera);
+    // labelRenderer.render(scene, camera);
+
     // Ecard.rotation.y += 0.005
     // card.rotation.y += -0.0061
 
